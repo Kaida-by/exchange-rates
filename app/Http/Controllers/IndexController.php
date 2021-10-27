0327d8ccac6ef8992a52c0c653a5d4e5595f165a
Денис Столяров <skwidw92@gmail.com>
2021-10-27 14:48:41 +0300

    feat: add IndexController

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bank;
+use App\Models\Currency;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class IndexController extends Controller
+{
+    public function index(Request $request)
+    {
+        $date = today()->subDays(1)->format('Y-m-d');
+
+        $currencies = DB::table('banks_currencies', 'bc')
+            ->select(
+                'currencies.name_currency',
+                'currencies.slug_currency',
+                'currencies.short_name_currency',
+                'banks.name_bank',
+                'banks.description_bank',
+                'banks.slug_bank',
+                'bc.price',
+                'bc.created_at',
+            )
+            ->leftJoin('currencies', 'currencies.id', '=', 'currency_id')
+            ->leftJoin('banks', 'banks.id', '=', 'bank_id')
+            ->where('bc.created_at', 'like', '%' . $date . '%')
+            ->get();
+
+        return view('welcome', compact('currencies'));
+    }
+}
