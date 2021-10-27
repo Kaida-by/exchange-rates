f8a0e134d18ac067d9effd3c2ee3a8cd894a98f1
Денис Столяров <skwidw92@gmail.com>
2021-10-27 17:49:30 +0300

    fix: change IndexController and add IndexSecvise

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,53 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Bank;
 use App\Models\Currency;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Services\IndexService;
+use Illuminate\Http\Response;
 
 class IndexController extends Controller
 {
-    public function index(Request $request)
+    public function index(): Response
     {
         $banks = Bank::all();
         $currencies = Currency::all();
+        $prices = IndexService::getPrises();
 
-        $date = today()->subDays(1)->format('Y-m-d');
+        $currencies_banks = IndexService::getCurrenciesBanks($prices);
 
-//        $currencies_banks = DB::table('banks_currencies', 'bc')
-//            ->select(
-//                'currencies.name_currency',
-//                'currencies.slug_currency',
-//                'currencies.short_name_currency',
-//                'banks.name_bank',
-//                'banks.description_bank',
-//                'banks.slug_bank',
-//                'bc.price',
-//                'bc.created_at',
-//            )
-//            ->leftJoin('currencies', 'currencies.id', '=', 'currency_id')
-//            ->leftJoin('banks', 'banks.id', '=', 'bank_id')
-//            ->where('bc.created_at', 'like', '%' . $date . '%')
-//            ->get();
-
-        $prices = DB::table('currencies', 'c')
-            ->select('*')
-            ->leftJoin('banks_currencies', 'c.id', '=', 'banks_currencies.currency_id')
-            ->where('banks_currencies.created_at', 'like', '%' . $date . '%')
-            ->get();
-
-        $currencies_banks = [];
-
-        foreach ($prices as $price) {
-            $currencies_banks[$price->currency_id]['name_currency'] = $price->name_currency;
-            $currencies_banks[$price->currency_id]['slug_currency'] = $price->slug_currency;
-            $currencies_banks[$price->currency_id]['short_name_currency'] = $price->short_name_currency;
-            $currencies_banks[$price->currency_id]['background'] = $price->background;
-            $currencies_banks[$price->currency_id]['created_at'] = $price->created_at;
-            $currencies_banks[$price->currency_id]['updated_at'] = $price->updated_at;
-            $currencies_banks[$price->currency_id]['price'][] = $price->price;
-        }
-
-        return view('content', compact('currencies_banks', 'banks', 'currencies'));
+        return new Response(view('content', compact(
+            'currencies_banks', 'banks', 'currencies'
+        )));
     }
 }
---
+++ app/Services/IndexService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services;
+
+use \Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+
+class IndexService
+{
+    public static function getCurrenciesBanks(Collection $collection): array
+    {
+        $tmp = [];
+
+        foreach ($collection as $price) {
+            $tmp[$price->currency_id]['currency_id'] = $price->currency_id;
+            $tmp[$price->currency_id]['bank_id'] = $price->bank_id;
+            $tmp[$price->currency_id]['name_currency'] = $price->name_currency;
+            $tmp[$price->currency_id]['slug_currency'] = $price->slug_currency;
+            $tmp[$price->currency_id]['short_name_currency'] = $price->short_name_currency;
+            $tmp[$price->currency_id]['background'] = $price->background;
+            $tmp[$price->currency_id]['created_at'] = $price->created_at;
+            $tmp[$price->currency_id]['updated_at'] = $price->updated_at;
+            $tmp[$price->currency_id]['price'][] = $price->price;
+        }
+
+        $currencies_banks = [];
+
+        foreach ($tmp as $price) {
+            $currencies_banks[$price['currency_id']] = $price;
+            foreach ($price['price'] as $key => $value) {
+                if (min($price['price']) === $value) {
+                    $currencies_banks[$price['currency_id']]['prises'][$key]['min'] = true;
+                } else {
+                    $currencies_banks[$price['currency_id']]['prises'][$key]['min'] = false;
+                }
+                $currencies_banks[$price['currency_id']]['prises'][$key]['value'] = $value;
+            }
+            unset($currencies_banks[$price['currency_id']]['price']);
+        }
+
+        return $currencies_banks;
+    }
+
+    public static function getPrises(): Collection
+    {
+        $date = today()->subDays(1)->format('Y-m-d');
+
+        return DB::table('currencies', 'c')
+            ->select('*')
+            ->leftJoin('banks_currencies', 'c.id', '=', 'banks_currencies.currency_id')
+            ->where('banks_currencies.created_at', 'like', '%' . $date . '%')
+            ->get();
+    }
+}
